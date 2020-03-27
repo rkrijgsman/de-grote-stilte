60f92a6b636f808a941f27019987ad43cec13ab2
rkrijgsman <robinkrijgsman96@gmail.com>
2020-03-27 15:37:34 +0100

    0.1.1
    toevoeging theater zaal

---
+++ degrotestilte/Assets/scripts/OnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+public class OnClick : MonoBehaviour
+{
+    public UnityEvent onClick;
+
+    private void OnMouseDown()
+    {
+        onClick.Invoke();
+    }
+}
---
+++ degrotestilte/Assets/scripts/audiopickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audiopickup : MonoBehaviour
+{
+    public bool audioplaying = false;
+            AudioSource audio;
+    public Rigidbody rb;
+    public AudioClip SoundToPlay;
+    public float Volume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.isKinematic == true && audioplaying == false)
+        {
+            audio.PlayOneShot(SoundToPlay, Volume);
+            audioplaying = true;
+        }
+        
+
+    }
+}
