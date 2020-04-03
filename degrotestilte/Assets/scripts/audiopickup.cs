0ee21e416db12ea89c619784734be84775ad0a9c
rkrijgsman <robinkrijgsman96@gmail.com>
2020-04-03 12:57:38 +0200

    1.0.6.4
    audio click

--- degrotestilte/Assets/scripts/audiopickup.cs
+++ degrotestilte/Assets/scripts/audiopickup.cs
@@ -2,30 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class audiopickup : MonoBehaviour
-{
+public class audiopickup : MonoBehaviour {
+
+
+    public Transform hand;
+    public Rigidbody rb;
+    
     public bool audioplaying = false;
             AudioSource audio;
-    public Rigidbody rb;
+ 
     public AudioClip SoundToPlay;
     public float Volume;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    // Use this for initialization
+    void Start () {
+
+
         rb = GetComponent<Rigidbody>();
         audio = GetComponent<AudioSource>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (rb.isKinematic == true && audioplaying == false)
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+       if (rb.isKinematic == true && audioplaying == false)
         {
             audio.PlayOneShot(SoundToPlay, Volume);
             audioplaying = true;
         }
+	}
+
+    private void OnMouseDown()
+    {
+        rb.useGravity = false;
+        transform.position = hand.position;
+        transform.parent = hand;
+        rb.isKinematic = true;
+     
         
 
     }
+
+  // private void OnMouseUp()
+  //  {
+  //      transform.parent = null;
+  //      rigidbody.useGravity = true;
+  //      rigidbody.isKinematic = false;
+  //      rigidbody.AddForce(Camera.main.transform.forward * 15, ForceMode.Impulse);
+  //  }
 }
---
+++ degrotestilte/Assets/scripts/clickaudio.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickaudio : MonoBehaviour {
+
+
+ 
+  
+     public bool audioplaying = false;
+            AudioSource audio;
+ 
+    public AudioClip SoundToPlay;
+    public float Volume;
+    
+
+    // Use this for initialization
+    void Start () {
+
+
+      
+        audio = GetComponent<AudioSource>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+   
+       
+	}
+
+    private void OnMouseDown()
+    {
+
+      if (audioplaying == false)
+        {
+            GetComponent<AudioSource>().PlayOneShot(SoundToPlay, Volume);
+            audioplaying = true;
+        }
+        audioplaying = false;
+   
+     
+        
+
+    }
+
+  // private void OnMouseUp()
+  //  {
+  //      transform.parent = null;
+  //      rigidbody.useGravity = true;
+  //      rigidbody.isKinematic = false;
+  //      rigidbody.AddForce(Camera.main.transform.forward * 15, ForceMode.Impulse);
+  //  }
+}
