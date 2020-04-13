15e284de47ba5e20efaabc5a7926e74d4e888725
rkrijgsman <robinkrijgsman96@gmail.com>
2020-04-13 21:36:57 +0200

    1.0.9.5
    -papegaaitjes, boom, veel tweaks aan scène overgangen en snelheden
    -edits aan scriptjes en timing
    -alle zwaartekrachten van stoelen en ruimte ingesteld en aan triggers toegevoegd

---
+++ degrotestilte/Assets/scripts/addforcefirstwall.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addforcefirstwall : MonoBehaviour
+{
+    public float thrustx, thrusty, thrustz;
+
+    public Rigidbody rb;
+    public float xAngle, yAngle, zAngle;
+    public Transform obj;
+
+     public float waittime;
+  
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           StartCoroutine(waiter());
+        rb = GetComponent<Rigidbody>();
+       
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator waiter()
+{
+
+    yield return new WaitForSeconds(waittime);
+     
+    rb.isKinematic = false;
+ 
+   rb.AddForce(thrustx, thrusty, thrustz, ForceMode.Impulse);
+   obj.transform.eulerAngles = new Vector3(
+    obj.transform.eulerAngles.x + xAngle,
+    obj.transform.eulerAngles.y + yAngle,
+    obj.transform.eulerAngles.z + zAngle);
+    
+    
+
+    
+}
+    }
+
