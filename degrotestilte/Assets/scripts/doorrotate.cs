e1fd6fea5296e04397888d23fc6b30b612bf35cc
rkrijgsman <robinkrijgsman96@gmail.com>
2020-04-14 13:08:32 +0200

    1.0.9.6
    -werkende deur
    -eerste WASD tekst

---
+++ degrotestilte/Assets/scripts/doorrotate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorrotate : MonoBehaviour
+{
+    
+
+    public float xAngle, yAngle, zAngle;
+    public Transform obj;
+        public float waittime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    
+   StartCoroutine(waiter());
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      obj.transform.eulerAngles = new Vector3(
+    obj.transform.eulerAngles.x + xAngle,
+    obj.transform.eulerAngles.y + yAngle,
+    obj.transform.eulerAngles.z + zAngle);
+    }
+     IEnumerator waiter()
+{
+
+
+    yield return new WaitForSeconds(waittime);
+    gameObject.GetComponent<doorrotate>().enabled = false;
+    
+}
+    }
+
