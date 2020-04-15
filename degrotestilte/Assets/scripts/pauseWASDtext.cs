4a1b09d8e93152a310095d5467cdd9b924ba72b5
rkrijgsman <robinkrijgsman96@gmail.com>
2020-04-15 16:17:41 +0200

    1.0.9.7
    huge changes:
    -veel models toegevoegd
    -cafe tutorial gedesigned

---
+++ degrotestilte/Assets/scripts/pauseWASDtext.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseWASDtext : MonoBehaviour
+
+{
+  
+ 
+    public float lengthtext;
+    void Start()
+    {
+     
+var videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer.Pause();
+    }
+     void Update()
+        {
+         var videoPlayer = GetComponent<UnityEngine.Video.VideoPlayer>();
+        
+
+         if (Input.GetKeyDown(KeyCode.W))
+         {
+             videoPlayer.Play();
+              StartCoroutine(waiter());
+         }
+         if (Input.GetKeyDown(KeyCode.A))
+         {
+             videoPlayer.Play();
+              StartCoroutine(waiter());
+          
+              
+         }
+         if (Input.GetKeyDown(KeyCode.S))
+         {
+             videoPlayer.Play();
+              StartCoroutine(waiter());
+         }
+         if (Input.GetKeyDown(KeyCode.D))
+         {
+             videoPlayer.Play();
+              StartCoroutine(waiter());
+         }
+         }
+    
+    IEnumerator waiter()
+{
+    
+    yield return new WaitForSeconds(lengthtext);
+    
+        Destroy(gameObject);
+    
+
+    
+}
+}
