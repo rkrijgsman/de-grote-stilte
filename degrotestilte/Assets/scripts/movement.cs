a15366f89ffefeb029f764eb977668c843ea87d8
rkrijgsman <robinkrijgsman96@gmail.com>
2020-04-16 15:51:45 +0200

    1.0.9.9
    -nieuwe tekst
    -nieuwe movement en wait move scripts

--- degrotestilte/Assets/scripts/movement.cs
+++ degrotestilte/Assets/scripts/movement.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    public float xmove;
-    public float ymove;
-    public float zmove;
+    public float xmove, ymove, zmove;
+ 
+    public float xAngle, yAngle, zAngle;
+
+   public Transform obj;
     // Start is called before the first frame update
     void Start()
     {
-     
+    
     }
 
     // Update is called once per frame
     void Update()
     {
            transform.Translate(xmove, ymove, zmove);
+            obj.transform.eulerAngles = new Vector3(
+    obj.transform.eulerAngles.x + xAngle,
+    obj.transform.eulerAngles.y + yAngle,
+    obj.transform.eulerAngles.z + zAngle);
      
     }
 }
---
+++ degrotestilte/Assets/scripts/waitbeforemove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waitbeforemove : MonoBehaviour
+{
+              public Transform obj;
+    public float xmove, ymove, zmove;
+   
+public float xAngle, yAngle, zAngle;
+    private bool wait = true;
+
+    public float waitsbeforemove;
+ 
+    void Start()
+    {
+
+     StartCoroutine(waiter());
+     
+
+    }
+    void Update()
+    {
+        if (wait == false){
+        transform.Translate(xmove, ymove, zmove);
+        obj.transform.eulerAngles = new Vector3(
+    obj.transform.eulerAngles.x + xAngle,
+    obj.transform.eulerAngles.y + yAngle,
+    obj.transform.eulerAngles.z + zAngle);
+    }
+}
+  
+
+    
+    IEnumerator waiter()
+{
+   
+    yield return new WaitForSeconds(waitsbeforemove);
+  wait = false;
+ 
+        
+    
+
+    
+}
+}
