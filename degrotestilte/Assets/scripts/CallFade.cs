40b5bd9413945361b660ed908f840c13ad8aef41
rkrijgsman <robinkrijgsman96@gmail.com>
2020-05-15 14:36:13 +0200

    1.9.2
    audio fade

---
+++ degrotestilte/Assets/scripts/CallFade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallFade : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float duration;
+    public float targetVolume;
+
+    void Start()
+    {
+        StartCoroutine(FadeAudioSource.StartFade(audioSource, duration, targetVolume));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
