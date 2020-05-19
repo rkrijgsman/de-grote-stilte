23389251a9169dac87c42734cff6684d250d0590
rkrijgsman <robinkrijgsman96@gmail.com>
2020-05-19 14:42:50 +0200

    1.9.5
    - colliders in kooi deels beter
    - brug op berg delen toegevoegd
    - black screen intro fade

---
+++ degrotestilte/Assets/scripts/SubtitleDelay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SubtitleDelay : MonoBehaviour
+{
+      public float waitsbeforeimage;
+
+    public float waitsbeforeimagedestroy;
+    public Image sub;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(waiter());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     IEnumerator waiter()
+{
+    yield return new WaitForSeconds(waitsbeforeimage);
+    
+sub.enabled = true;
+yield return new WaitForSeconds(waitsbeforeimagedestroy);
+
+    Destroy(gameObject);
+}
+}
---
+++ degrotestilte/Assets/scripts/waitanddestroy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waitanddestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float waitsbeforedestroy;
+    void Start()
+    {
+        StartCoroutine(waiter());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     IEnumerator waiter()
+{
+    yield return new WaitForSeconds(waitsbeforedestroy);
+     Destroy(gameObject);
+}
+}
