993ad0da624f4b301ef8d67d02aefbd78b899b70
rkrijgsman <robinkrijgsman96@gmail.com>
2020-05-27 13:02:38 +0200

    2.0.4
    Scene2 gestart, deze update is vooral voor de merge

---
+++ degrotestilte/Assets/scripts/FadeInOut.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FadeInOut : MonoBehaviour
+{
+    public float max = 1f;
+public float speed = 5.0f;
+
+public SpriteRenderer sprite;
+
+void Update () {
+    sprite.color = new Color(1f, 1f, 1f, Mathf.PingPong(Time.time * speed, max));
+}
+}   
