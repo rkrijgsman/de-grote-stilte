2e50b12898e30d5b70de22e674e1f165f6f18cce
rkrijgsman <robinkrijgsman96@gmail.com>
2020-06-26 13:34:04 +0200

    2.2.4
    hotel 3 keer

---
+++ degrotestilte/Assets/scripts/scriptnaarscene2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptnaarscene2 : MonoBehaviour
+{
+    public string scene;
+    public Color loadToColor = Color.black;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initiate.Fade(scene,loadToColor,1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
